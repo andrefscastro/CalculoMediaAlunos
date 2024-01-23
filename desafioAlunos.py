2d06e263b47d0097908ece92c749b75c313c49cd
André Felipe Castro <67480773+andrefscastro@users.noreply.github.com>
2024-01-23 16:40:42 -0400

    Adicionando os arquivos do programa.

---
+++ desafioAlunos.py
@@ -0,0 +1,26 @@
+import json
+
+##programa para calcular a media dos alunos buscandos os dados em um arquivo JSON
+
+with open('AlunosNota.json', 'r', encoding="utf-8") as file:
+    dados = json.load(file)
+
+for estudante in dados['Alunos']:
+    notas = estudante["notas"]
+    mediaNotas = sum(notas)/len(notas)
+    estudante["media"] = mediaNotas
+
+with open('AlunosNota.json', 'w', encoding="utf-8") as file:
+    json.dump(dados, file, indent=2)
+
+
+##gravar dados de estudantes aprovados e reprovados
+
+with open('AlunosAprovados.json', 'w') as fileabove, \
+    open('AlunosReprovados.json', 'w') as filebelow:
+
+    estudantesAprovados = [s for s in dados["Alunos"] if s["media"] >= 70]
+    estudantesReprovados = [s for s in dados["Alunos"] if s["media"] < 70]
+
+    json.dump({"Alunos": estudantesAprovados}, fileabove)
+    json.dump({"Alunos": estudantesReprovados}, filebelow)
